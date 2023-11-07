d12cfcc78250dbbe65783d847fc0b84465b055d9
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-11-07 08:58:03 +0100

    commit quitar el weather que viene por defecto

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Proyecto_MAUI_Reto_parte_1.Data;
 
 namespace Proyecto_MAUI_Reto_parte_1;
 
@@ -21,8 +20,6 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
-
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
