ff062ac9095226424b69d50231107d1175235cf5
Sergio <sergio.galerafr@elorrieta-errekamari.com>
2023-11-07 10:48:50 +0100

    WeatherApp Finalizado y funcional

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -15,12 +15,12 @@ namespace Proyecto_MAUI_Reto_parte_1.Models
         public string timezone { get; set; }
         public string timezone_abbreviation { get; set; }
         public float elevation { get; set; }
-        public Current_Units current_units { get; set; }
-        public Current current { get; set; }
-        public Hourly_Units hourly_units { get; set; }
-        public Hourly hourly { get; set; }
-        public Daily_Units daily_units { get; set; }
-        public Daily daily { get; set; }
+        public Current_Units_Weather current_units { get; set; }
+        public Current_Weather current { get; set; }
+        public Hourly_Units_Weather hourly_units { get; set; }
+        public Hourly_Weather hourly { get; set; }
+        public Daily_Units_Weather daily_units { get; set; }
+        public Daily_Weather daily { get; set; }
     }
 
     public class Current_Units_Weather
