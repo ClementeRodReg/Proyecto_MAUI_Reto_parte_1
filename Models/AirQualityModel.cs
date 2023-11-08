5f03f67a4e4cbd25986d7621e52e7983b4925ba1
Sergio <sergio.galerafr@elorrieta-errekamari.com>
2023-11-08 08:17:24 +0100

    AirQualityModels

--- Models/AirQualityModel.cs
+++ Models/AirQualityModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace Proyecto_MAUI_Reto_parte_1.Models
 {
-    internal class AirQualityModel
+    public class AirQualityModel
     {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public float elevation { get; set; }
+        public Current_Units current_units { get; set; }
+        public Current current { get; set; }
     }
+
+    public class Current_Units_Air
+    {
+        public string time { get; set; }
+        public string interval { get; set; }
+        public string pm10 { get; set; }
+        public string pm2_5 { get; set; }
+        public string carbon_monoxide { get; set; }
+        public string nitrogen_dioxide { get; set; }
+        public string sulphur_dioxide { get; set; }
+        public string ozone { get; set; }
+    }
+
+    public class Current_Air
+    {
+        public string time { get; set; }
+        public int interval { get; set; }
+        public float pm10 { get; set; }
+        public float pm2_5 { get; set; }
+        public float carbon_monoxide { get; set; }
+        public float nitrogen_dioxide { get; set; }
+        public float sulphur_dioxide { get; set; }
+        public float ozone { get; set; }
+    }
+
 }
+apps - fileview.texmex_20231026.01_p0
+AirQualiryModel.txt
+Mostrando AirQualiryModel.txt.
